d258bee7274f292febfd72ba769a3261ebc30d95
henk687 <henkleenheer@upcmail.nl>
2021-01-15 15:29:07 +0100

    changes

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -3,8 +3,8 @@ import ImageTwo from '../images/huis2.jpg'
 
 export const InfoData = { 
         heading: 'Vietnam', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta quod architecto, eum magnam aspernatur?', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laboriosam fugit rem quo repellat aut?',
+        paragraphOne: 'Vietnam, officieel de Socialistische Republiek Vietnam, is een land in Zuidoost-Azië. Met 98.721.275 inwoners. De hoofdstad is het noordelijke Hanoi en de grootste stad is het zuidelijke Ho Chi Minhstad, het vroegere Saigon.', 
+        paragraphTwo: 'Vietnam grenst in het noorden aan China (1281 km), in het noordwesten aan Laos (2130 km) en in het zuidwesten aan Cambodja (950 km). De maximale afstand van noord naar zuid is 1650 kilometer en de breedte van het land varieert van maximaal 600 kilometer tot 50 kilometer in het midden van het land tussen Laos en de Zuid-Chinese Zee. De totale kustlijn is meer dan 2500 kilometer lang en loopt van noord naar zuid langs de Golf van Tonkin, de Zuid-Chinese Zee of Bien Dong en de Golf van Thailand. Voor een groot deel zijn dit natuurlijke grenzen, want Vietnam wordt van Laos en Cambodja gescheiden door het Truong Son-gebergte.Tot het grondgebied van Vietnam behoort een keten van duizenden eilandjes en bovendien claimt de regering nog een aantal andere eilanden, waaronder de Paraceleilanden (400 kilometer ten oosten van Da Nang) en de Spratleyeilanden (500 kilometer van de zuidoostkust).Het grootste eiland van Vietnam, Phu Quoc, ligt in de Golf van Thailand, net buiten de territoriale wateren van Cambodja, op 45 kilometer afstand van Ha Tien. Phu Quoc is maximaal 45 kilometer lang en 30 kilometer breed en heeft een oppervlakte van ca. 1300 km2. Phu Quoc behoort tot de uit 16 eilanden bestaande archipel Dao Phu Quoc.',
         buttonLabel: 'Lees Meer',
         buttonLink: '/bestemmingen',
         image: ImageOne, 
@@ -29,8 +29,8 @@ export const InfoData = {
 
     export const InfoDataThree = { 
         heading: 'Hanoi', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'Hanoi is de hoofdstad van Vietnam, alsmede een zelfstandige gemeente met de status van een provincie. De stad heeft ca. 7,7 miljoen inwoners en is de op een na grootste stad van het land, na Ho Chi Minhstad.', 
+        paragraphTwo: 'Hanoi is de hoofdstad van Vietnam, maar staat ook bekend om haar dorpse karakter. In het knusse oude centrum kun je heerlijk slenteren, shoppen en eten, en ook in de rest van deze aangename stad valt een hoop te zien en te doen. Neem bijvoorbeeld een duik in het luisterrijke verleden van Vietnam, bezoek een waterpoppenshow of ga een dagje de natuur in.',
         buttonLabel: 'Bekijk Afbeeldingen',
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
@@ -44,8 +44,8 @@ export const InfoData = {
 
     export const InfoDataFour= { 
         heading: 'Sapa', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'Sa Pa is een thị trấn in het district Sa Pa in de Vietnamese provincie Lào Cai. Sa Pa ligt in de bergen in de regio Noord-Vietnam. De thị trấn is populair bij toeristen vanwege het schitterende landschap en het Black-Hmong volk dat er leeft.', 
+        paragraphTwo: 'In het noordwesten van Vietnam, tegen de grens van China, vind je het stadje Sapa. Omgeven door de Hoang Lien bergen en gelegen in een vallei, draait alles hier om de prachtige omgeving. Een trekking door het gebied zorgt voor een schitterend uitzicht op het dal en de Fansipan; de hoogste berg van Vietnam. Je vindt er terrasvormige rijstvelden die tegen bergen aan liggen, leuke dorpjes en heel veel groen. Sapa is de ideale uitvalsbasis om deze regio te verkennen. Je kunt vanuit hier een trekking maken door de verschillende dorpen en langs de bijzondere rijstterrassen.',
         buttonLabel: 'Bekijk Afbeeldingen',
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
@@ -59,8 +59,8 @@ export const InfoData = {
 
     export const InfoDataFive = { 
         heading: 'Halong Bay', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'De Ha Longbaai is een baai bij de stad Hạ Long in het noorden van Vietnam in de Golf van Tonkin, nabij de grens met China. De baai heeft een kustlijn van 120 kilometer en het gebied beslaat omgeveer 1500 vierkante kilometer. Vịnh Hạ Long betekent in het Vietnamees Baai van de dalende draak.', 
+        paragraphTwo: 'In de Golf van Tonkin ligt een absoluut wereldwonder. Meer dan 1600 kalkstenen eilandjes rijzen uit de zee en vormen zo de machtige Halong Bay. Het landschap is een mysterieus en adembenemend schouwspel en staat dus niet voor niets op de UNESCO werelderfgoedlijst.',
         buttonLabel: 'Bekijk Afbeeldingen',
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
@@ -74,8 +74,8 @@ export const InfoData = {
 
     export const InfoDataSix = { 
         heading: 'Ninh Binh ', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'Ninh Bình is een provincie van Vietnam. Ninh Bình telt 928.500 inwoners op een oppervlakte van 1392 km².', 
+        paragraphTwo: 'Ninh Binh wordt ook wel het ‘Halong Bay van het vaste land’ genoemd. Het staat bekend om de kalksteen rotsen, de felgroene rijstvelden, rivieren, prachtige uitzichten en verschillende oude tempels.',
         buttonLabel: 'Bekijk Afbeeldingen',
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
@@ -89,8 +89,8 @@ export const InfoData = {
 
     export const InfoDataSeven = { 
         heading: 'Mai Chau', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'In het berggebied van de Hao Binh provincie ligt de Mai Chau vallei. Gelegen op een afstand van 135 kilometer ten zuidoosten van Hanoi.', 
+        paragraphTwo: 'Mai Chau is gelegen in een rustige, groene vallei vol met rijstvelden en boerderijen met een eigen traditionele bouwstijl. In de lente is de vallei prachtig gekleurd door de vele verschillende bloemensoorten die in bloei staan, zoals de perzikbloesem. In Mai Chau vindt u meerdere etnische volkeren zoals de Witte Thai en de H’mong. Van het gebied rond Mai Chau wordt wel gezegd dat het de geboorteplaats is van de echte Vietnamees en het is werkelijk prachtig.',
         buttonLabel: 'Bekijk Afbeeldingen',
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
@@ -104,8 +104,8 @@ export const InfoData = {
 
     export const InfoDataEight = { 
         heading: 'Hué', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'Huế is een stad gelegen in centraal Vietnam in de provincie Thừa Thiên-Huế, waarvan het ook de hoofdstad is. De stad heeft ongeveer 340.000 inwoners.', 
+        paragraphTwo: 'Hué is de stad waar de Vietnamese cultuur zijn oorsprong kent en waar deze het best bewaard is gebleven. Het was de keizerlijke hoofdstad in de Nguyen dynastie van 1802 tot 1945. Hué is de stad van kunst, architectonische hoogstandjes en religie.',
         buttonLabel: 'Bekijk Afbeeldingen', 
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
@@ -119,8 +119,8 @@ export const InfoData = {
 
     export const InfoDataNine = { 
         heading: 'Hoi An', 
-        paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi unde id numquam eos laboriosam dicta ', 
-        paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. fugit rem quo repellat aut?',
+        paragraphOne: 'Hội An is een kuststad in centraal Vietnam aan de Zuid-Chinese Zee. De stad ligt in de provincie Quảng Nam en heeft ongeveer 120.000 inwoners. Deze voormalige havenstad van het koninkrijk Champa ligt aan het estuarium van de Hội An en was een belangrijk handelscentrum in de 16e en 17e eeuw.', 
+        paragraphTwo: 'Het leukste aan Hoi An is de gezellige sfeer die in de stad heerst. De stad was vroeger een belangrijke havenstad in Vietnam, maar tegenwoordig heeft het geen haven meer. Toch zijn de invloeden nog te zien. Zo zie je nog verschillende koopmanshuizen die vroeger voor de handel gebruikt werden. Ze zijn nog steeds in goede staat. Het centrum van de stad autovrij, wat de sfeer ook ten goede komt. Na zonsondergang gaan de lampen aan en verfraaien de kleurrijke lampionnen de straten. Het is dan ook leuk om ‘s avonds de Night Market te bezoeken. Verder zijn er een aantal leuke bars en restaurants te vinden in Hoi An. De goede sfeer en de leuke bars maken het een leuke stad om uit te gaan.',
         buttonLabel: 'Bekijk Afbeeldingen', 
         buttonLink: '/afbeeldingen',
         image: ImageTwo, 
