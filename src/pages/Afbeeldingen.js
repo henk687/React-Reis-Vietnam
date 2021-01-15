9a87aaab96c7541ff32c83aa872280a7f0e98c4d
henk687 <henkleenheer@upcmail.nl>
2021-01-15 10:39:13 +0100

    changes

--- src/pages/Afbeeldingen.js
+++ src/pages/Afbeeldingen.js
@@ -1,27 +1,12 @@
 import React from 'react'
 import Features from '../components/Features'
-import Hero from '../components/Hero'
-import { FeaturesDataEight, FeaturesDataFive, FeaturesDataFour, FeaturesDataSeven, FeaturesDataSix, FeaturesDataThree, FeaturesDataTwo } from '../data/FeaturesData'
-import { SliderDataHalongbay, SliderDataHanoi, SliderDataMaichau, SliderDataHue, SliderDataHoian, SliderDataNinhbinh, SliderDataSapa } from '../data/SliderData'
+import { FeaturesDataTwo } from '../data/FeaturesData'
 
 
 const Afbeeldingen = () => {
     return (
         <>
             <Features {...FeaturesDataTwo}/>
-            <Hero slides={SliderDataHanoi} />
-            <Features {...FeaturesDataThree}/>
-            <Hero slides={SliderDataSapa} />
-            <Features {...FeaturesDataFour}/>
-            <Hero slides={SliderDataHalongbay} />
-            <Features {...FeaturesDataFive}/>
-            <Hero slides={SliderDataNinhbinh} />
-            <Features {...FeaturesDataSix}/>
-            <Hero slides={SliderDataMaichau} />
-            <Features {...FeaturesDataSeven}/>
-            <Hero slides={SliderDataHue} />
-            <Features {...FeaturesDataEight}/>
-            <Hero slides={SliderDataHoian} />
         </>
     )
 }
