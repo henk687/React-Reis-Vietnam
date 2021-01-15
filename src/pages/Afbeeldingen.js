48b1ae172fb0ab05f00154683524f291c7485951
henk687 <henkleenheer@upcmail.nl>
2021-01-15 14:19:33 +0100

    change

--- src/pages/Afbeeldingen.js
+++ src/pages/Afbeeldingen.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import Features from '../components/Features'
-import Hero from '../components/Hero'
-import { FeaturesDataThree, FeaturesDataTwo } from '../data/FeaturesData'
-import { SliderDataHanoi } from '../data/SliderData'
+import { FeaturesDataTwo } from '../data/FeaturesData'
 
 
 const Afbeeldingen = () => {
     return (
         <>
-            <Features {...FeaturesDataTwo}/>
-            <Hero slides={SliderDataHanoi} />
             <Features {...FeaturesDataThree}/>
         </>
     )
