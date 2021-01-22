0a6ab1f47ca83843d08634e64bc0c5a0ac4d410d
henk687 <henkleenheer@upcmail.nl>
2021-01-22 17:49:49 +0100

    changes

--- src/pages/Afbeeldingen.js
+++ src/pages/Afbeeldingen.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Features from '../components/Features'
+import Hero from '../components/Hero'
 import { FeaturesDataThree } from '../data/FeaturesData'
+import { SliderData } from '../data/SliderData'
 
 
 const Afbeeldingen = () => {
     return (
         <>
             <Features {...FeaturesDataThree}/>
+            <Hero slides={SliderData}/>
         </>
     )
 }
